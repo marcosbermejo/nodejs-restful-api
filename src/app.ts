d1e49af3c393390c6e41cd20fbeba7d8239c75c2
Marcos Bermejo <marcosbermejom@gmail.com>
2023-08-31 11:16:02 +0200

    feat: add helmet middleware for security

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import helmet from 'helmet';
 
 const app: Application = express();
+
+app.use(helmet());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-export default app
+export default app;
--- test/integration/app.test.ts
+++ test/integration/app.test.ts
@@ -8,4 +8,20 @@ describe('Integration Tests for the Express Application', () => {
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello, World!');
   });
+
+  it('Should respond add security headers', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.headers).toMatchObject({
+      'x-dns-prefetch-control': 'off',
+      'x-frame-options': 'SAMEORIGIN',
+      'x-content-type-options': 'nosniff',
+    });
+  });
+
+  it('Should not include x-powered-by header', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.headers['x-powered-by']).toBeUndefined();
+  });
 });
