6f0c5f4e86f9ecc9b6f1a1202790497a994118a7
Marcos Bermejo <marcosbermejom@gmail.com>
2023-09-03 14:26:23 +0200

    fix: sonarcloud issues

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -17,14 +17,17 @@ router.get('/:id', validate(getTeamValidation), async (req: Request, res: Respon
 
 router.get('/', validate(getTeamsValidation), async (req: Request, res: Response) => {
   const { clubId } = req.query;
-  const teams = await Team.find(clubId ? { club: clubId } : {});
+  const filter = clubId ? { club: clubId.toString() } : {};
+  const teams = await Team.find(filter);
 
   return res.status(200).json(teams);
 });
 
 router.post('/', validate(createTeamValidation), async (req: Request, res: Response) => {
   const { name, club } = req.body;
-  const newTeam = await Team.create({ name, club });
+  const teamData = { name: name.toString(), club: club.toString() };
+  const newTeam = await Team.create(teamData);
+
   return res.status(201).json(newTeam);
 });
 
